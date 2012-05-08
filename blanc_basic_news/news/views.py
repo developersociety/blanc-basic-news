f01222f021f277805492e3f539609f6b64be0b7e
Alex Tomkins <tomkins@darkzone.net>
2012-05-08 15:32:38 +0100

    Use select_related to help with category foreign keys

--- blanc_basic_news/news/views.py
+++ blanc_basic_news/news/views.py
@@ -9,7 +9,8 @@ class PostListView(ListView):
     paginate_by = getattr(settings, 'NEWS_PER_PAGE', 10)
 
     def get_queryset(self):
-        return Post.objects.filter(published=True, date__lte=timezone.now())
+        return Post.objects.select_related().filter(
+                published=True, date__lte=timezone.now())
 
 
 class PostListCategoryView(ListView):
@@ -18,7 +19,10 @@ class PostListCategoryView(ListView):
 
     def get_queryset(self):
         self.category = get_object_or_404(Category, slug=self.kwargs['slug'])
-        return Post.objects.filter(published=True, date__lte=timezone.now(), category=self.category)
+        return Post.objects.select_related().filter(
+                published=True,
+                date__lte=timezone.now(),
+                category=self.category)
 
     def get_context_data(self, **kwargs):
         context = super(PostListCategoryView, self).get_context_data(**kwargs)
