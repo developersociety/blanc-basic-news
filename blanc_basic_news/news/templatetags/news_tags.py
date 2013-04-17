11245afe9a46e8dbc025b09fba1c59bcba2f7960
Alex Tomkins <tomkins@darkzone.net>
2013-04-17 16:54:40 +0100

    get_latest_news can now have a category filter

--- blanc_basic_news/news/templatetags/news_tags.py
+++ blanc_basic_news/news/templatetags/news_tags.py
@@ -16,6 +16,12 @@ def get_news_months():
 
 
 @register.assignment_tag
-def get_latest_news(count):
-        return Post.objects.select_related().filter(
-                published=True, date__lte=timezone.now())[:count]
+def get_latest_news(count, category=None):
+    post_list = Post.objects.select_related().filter(
+        published=True, date__lte=timezone.now())
+
+    # Optional filter by category
+    if category is not None:
+        post_list = post_list.filter(category__slug=category)
+
+    return post_list[:count]
