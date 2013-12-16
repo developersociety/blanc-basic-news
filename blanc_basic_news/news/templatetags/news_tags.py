bff57211f829c016122c3856606b25a4fac76de4
Alex Tomkins <tomkins@darkzone.net>
2013-12-16 17:07:49 +0000

    Template tags didn't work with custom models

--- blanc_basic_news/news/templatetags/news_tags.py
+++ blanc_basic_news/news/templatetags/news_tags.py
@@ -1,6 +1,7 @@
 from django import template
 from django.utils import timezone
-from blanc_basic_news.news.models import Category, Post
+from blanc_basic_news.news import get_post_model
+from blanc_basic_news.news.models import Category
 
 register = template.Library()
 
@@ -12,13 +13,13 @@ def get_news_categories():
 
 @register.assignment_tag
 def get_news_months():
-    return Post.objects.filter(
+    return get_post_model().objects.filter(
         published=True, date__lte=timezone.now()).dates('date', 'month')
 
 
 @register.assignment_tag
 def get_latest_news(count, category=None):
-    post_list = Post.objects.select_related().filter(
+    post_list = get_post_model().objects.select_related().filter(
         published=True, date__lte=timezone.now())
 
     # Optional filter by category
