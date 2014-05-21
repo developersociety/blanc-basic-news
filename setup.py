3e024d8e0d1cfa4812a76090c63c514e6dbb4ec9
Alex Tomkins <tomkins@darkzone.net>
2014-05-21 12:19:17 +0100

    Update setup.py, using setuptools only, add additional Python versions

--- setup.py
+++ setup.py
@@ -1,29 +1,27 @@
 #!/usr/bin/env python
-try:
-    from setuptools import setup
-except ImportError:
-    from distutils.core import setup
-
+from setuptools import setup, find_packages
 
 setup(
     name='blanc-basic-news',
     version='0.2.3',
     description='Blanc Basic News for Django',
     long_description=open('README.rst').read(),
-    url='http://www.blanctools.com/',
+    url='https://github.com/blancltd/blanc-basic-news',
     maintainer='Alex Tomkins',
-    maintainer_email='alex@hawkz.com',
+    maintainer_email='alex@blanc.ltd.uk',
     platforms=['any'],
-    packages=[
-        'blanc_basic_news',
-        'blanc_basic_news.templatetags',
-    ],
+    packages=find_packages(),
     classifiers=[
         'Environment :: Web Environment',
         'Framework :: Django',
         'License :: OSI Approved :: BSD License',
         'Operating System :: OS Independent',
         'Programming Language :: Python',
+        'Programming Language :: Python :: 2',
+        'Programming Language :: Python :: 2.7',
+        'Programming Language :: Python :: 3',
+        'Programming Language :: Python :: 3.2',
+        'Programming Language :: Python :: 3.3',
     ],
-    license='BSD-2',
+    license='BSD',
 )
